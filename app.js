46eef44835e983864547c0d53a963c0dcfe1147a
kimhyeji <gpwl5589@gmail.com>
2023-06-26 19:32:41 +0900

    세팅

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About!!");
+});
+
+app.get("/projects", (req, res) => {
+  res.send("projects!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
