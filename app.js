e431b252b628c8f257dcdb617d9ceab4b22e4ceb
kimhyeji <gpwl5589@gmail.com>
2023-06-26 20:33:36 +0900

    데이터 리스트 조회, 단건 조회

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "korad",
+  password: "kor123414",
+  database: "music_list",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 const app = express();
 const port = 3000;
@@ -14,8 +25,24 @@ const musicList = [
   },
 ];
 
-app.get("/music", (req, res) => {
-  res.json(musicList);
+app.get("/music_list", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM music_list ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.get("/music_list/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query("SELECT * FROM music_list WHERE id = ?", [
+    id,
+  ]);
+
+  if (rows.length == 0) {
+    res.status(404).send("not found");
+    return;
+  }
+
+  res.json(rows[0]);
 });
 
 app.listen(port, () => {
